300b2a9be70cf0650a2789ba72906bf9187443cb
Jon Cates <jcates2685@gmail.com>
2018-04-20 14:22:03 -0500

    Added word suggestions and basic input validation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -9,6 +10,19 @@ def translate(w):
 
     if w in data:
         return data[w]
+
+    elif len(get_close_matches(w, data.keys())) > 0:
+        confirm = input("Did you mean to type %s? Enter Y or N" % get_close_matches(w, data.keys())[0])
+
+        if confirm == "Y" or confirm == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+
+        elif confirm == "N" or confirm == "n":
+            return "This word doesn't exist. Please check your spelling."
+
+        else:
+            return "We didn't understand your input"
+
     else:
         return "This word doesn't exist. Please check your spelling."
 
@@ -17,4 +31,3 @@ word = input("Enter word: ")
 
 
 print(translate(word))
-
